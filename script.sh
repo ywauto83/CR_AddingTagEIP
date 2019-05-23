0d95bae0c0e17a35f4c11c7d5aee56be526f3e0b
ywauto83 <ywauto@gmail.com>
2019-05-23 20:27:23 +1000

    add steps how to push to a different repo

--- script.sh
+++ script.sh
@@ -8,3 +8,10 @@ aws cloudformation package \
 aws cloudformation deploy \--template-file /Users/waynyuan/Documents/my-local-repos/cfn/CR/AddingTagEIP/template_packaged.yaml \
 --stack-name myCRtestTagging \
 --capabilities CAPABILITY_IAM
+
+
+    # Create a new repo at github.
+    # Clone the repo from fedorahosted to your local machine.
+    # git remote rename origin upstream
+    # git remote add origin URL_TO_GITHUB_REPO
+    # git push origin master
